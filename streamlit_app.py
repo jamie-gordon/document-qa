a52f17e214343280d12cedc8b8b53f3546e00a03
jamie-gordon <94430945+jamie-gordon@users.noreply.github.com>
2024-11-13 00:19:49 +0000

    test-commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ st.title("📄 Document question answering")
 st.write(
     "Upload a document below and ask a question about it – GPT will answer! "
     "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
+    "Good luck!"
 )
 
 # Ask user for their OpenAI API key via `st.text_input`.
